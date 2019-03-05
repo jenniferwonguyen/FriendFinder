98950f657690d7c9dfdc8dbb097e3360c5c4ced7
Jennifer Wong <jenniferwonguyen@gmail.com>
2019-03-04 19:05:36 -0600

    file deploy

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Cardi B",
+		photo: "https://images.sk-static.com/images/media/profile_images/artists/8962314/huge_avatar",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+        name: "Gigi Hadid",
+        photo: "https://s.abcnews.com/images/Entertainment/gigi-hadid-gty-thg-180212_16x9_992.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Zendaya",
+		photo: "https://assets.teenvogue.com/photos/5c2fbc585337812d2a440476/1:1/w_1000,h_1000,c_limit/Zendaya%20Tout.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Miley Cyrus",
+		photo: "https://www.vagalume.com.br/miley-cyrus/images/miley-cyrus.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Selena Gomez",
+		photo: "https://m.media-amazon.com/images/M/MV5BMjAwNTE2MDMyMl5BMl5BanBnXkFtZTgwMjAyODM3MTI@._V1_UY317_CR12,0,214,317_AL_.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;

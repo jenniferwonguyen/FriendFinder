cdf0ab4dbd74590a37410bf0e2fc1afc41e04d32
Jennifer Wong <jenniferwonguyen@gmail.com>
2019-03-04 20:14:23 -0600

    fix trim

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,7 @@ var friends = require('../data/friends.js');
 
 // Export API routes
 module.exports = function(app) {
-	// console.log('___ENTER apiRoutes.js___');
+	
 
 	// Total list of friend entries
 	app.get('/api/friends', function(req, res) {
@@ -16,9 +16,7 @@ module.exports = function(app) {
 	// Add new friend entry
 	app.post('/api/friends', function(req, res) {
         // Capture the user input object
-        console.log("backend!!!!", req.body)
 		var userInput = req.body;
-		// console.log('userInput = ' + JSON.stringify(userInput));
 
 		var userResponses = userInput.scores;
 		// console.log('userResponses = ' + userResponses);
@@ -28,16 +26,14 @@ module.exports = function(app) {
 		var matchImage = '';
 		var totalDifference = 10000; // Make the initial value big for comparison
 
-		// Examine all existing friends in the list
+		// Loop all existing friends in the list
 		for (var i = 0; i < friends.length; i++) {
-			// console.log('friend = ' + JSON.stringify(friends[i]));
 
 			// Compute differenes for each question
 			var diff = 0;
 			for (var j = 0; j < userResponses.length; j++) {
 				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
 			}
-			// console.log('diff = ' + diff);
 
 			// If lowest difference, record the friend match
 			if (diff < totalDifference) {
@@ -53,8 +49,7 @@ module.exports = function(app) {
 
 		// Add new user
 		friends.push(userInput);
-        console.log("match name", matchName)
-		// Send appropriate response
+	
 		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
 	});
 };
